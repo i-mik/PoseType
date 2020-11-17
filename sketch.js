d35ca73b6845d2e11b99825285ea93a4e30dea7c
Michael <hux94handball@web.de>
2020-11-17 13:11:25 +0100

    Created Winner Signage and some code cleaning

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 let video;
 let poseNet;
 let pose;
-let treshHold = 100;
+let fadeOut;
+
+const treshHold = 100;
+const offSet = 5
+
+let valueOne;
+let valueTwo;
 
 function setup() {
   createCanvas(640, 480);
@@ -9,15 +15,56 @@ function setup() {
   video.hide();
   poseNet = ml5.poseNet(video, 'single', modelLoaded);
   poseNet.on('pose', gotPose);
+
+  // Set up the axes Settings from Bkg-Glyph to be put in the CSS-Property
+  valueOne = random(0, 100);
+  valueTwo = random(0, 100);
+  let newSettings = createCSSSettings(valueOne, valueTwo);
+  console.log("Current varSettings to be played are:", newSettings)
+  document.getElementById("bkgGlyph").style.setProperty("font-variation-settings", newSettings);
 }
+
 
 function modelLoaded() {
   console.log('poseNet ready!');
 }
 
+
 function gotPose(poses) {
   if (poses[0].pose.score >= 0.35) {
     pose = poses[0].pose;
+  }
+}
+
+
+// ################
+// Helper Functions
+// ################
+
+function checkValues(axisOne, axisTwo) {
+  let statment1 = false;
+  let statment2 = false;
+
+  if (axisOne >= (valueOne - offSet) && axisOne <= (valueOne + offSet)) {
+    statment1 = true;
+  }
+  if (axisTwo >= (valueTwo - offSet) && axisTwo <= (valueTwo + offSet)) {
+    statment2 = true;
+  }
+  /**console.log({
+      startValue1: valueOne,
+      currValue1: axisOne,
+      areClose1: statment1,
+      startValue2: valueTwo,
+      currValue2: axisTwo,
+      areClose2: statment2
+    })
+  **/
+  if (statment1 && statment2) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
 
@@ -32,45 +79,59 @@ function angleKeyPts(keyPtOne, keyPtTwo) {
   return degrees(atan2(keyPtOne.x, keyPtOne.y));
 }
 
-function drawFace() {
-  for (i = 0; i < 5; i++) {
-    let keyPt = pose.keypoints[i];
-    ellipse(keyPt.position.x, keyPt.position.y, 15);
-  }
+function createCSSSettings(valueAxisOne, valueAxisTwo) {
+  // Set up the axes Settings to be put in the CSS-Property
+  let varSettings = '"SLON" ' + valueAxisOne;
+  varSettings += ", ";
+  varSettings += '"SLTW" ' + valueAxisTwo;
+  return varSettings;
 }
 
+// ################
+// Drawing Function 
+// ################
 
 function draw() {
   image(video, 0, 0);
-  // filter(GRAY);
- // background(200)
 
   if (pose) {
-    // Calculate distance between Hands an map distance Value to Axes Value between 0 and 100
-    // let distHands = distKeyPts(pose.leftWrist, pose.rightWrist);
-    // distHands = map(distHands, 10, width - treshHold, 0, 100, true);
 
-    // Calculate Angle Between Both Hands
-     let angleHands = angleKeyPts(pose.leftWrist, pose.rightWrist);
-    console.log(angleHands);
+    let distHands = distKeyPts(pose.leftWrist, pose.rightWrist);
+    let axisOne = map(distHands, 50, width - 100, 0, 100, true)
+    // console.log(distHands, axisOne);
 
     // Map the second Axes value to Nose-x position
-    let nosePosition = map(pose.nose.x, 0, height, 0, 100, true);
+    let axisTwo = map(pose.nose.y, 50, height / 2 - 50, 0, 100, true);
 
-    // Set up the axes Settings to be put in the CSS-Property
-    let varSettings = '"SLON" ' + angleHands;
-    varSettings += ", ";
-    varSettings += '"SLTW" ' + nosePosition;
-    document.getElementById("sampleGlyph").style.setProperty("font-variation-settings", varSettings);
-    // console.log(varSettings)
+    newSettings = createCSSSettings(axisOne, axisTwo);
+    document.getElementById("sampleGlyph").style.setProperty("font-variation-settings", newSettings);
 
     // Draw Ellipse at Nose and Wrist Positions
     strokeWeight(0);
     fill(255, 0, 0);
-    drawFace();
+    ellipse(pose.nose.x, pose.nose.y, 15)
 
     fill(0, 0, 255);
-    ellipse(pose.leftWrist.x, pose.leftWrist.y, 15)
-    ellipse(pose.rightWrist.x, pose.rightWrist.y, 15)
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 15);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 15);
+
+    noFill();
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    rect(50, 50, width - 100, height / 2);
+
+    if (checkValues(axisOne, axisTwo)) {
+      fadeOut = 255;
+    }
+
+    if (fadeOut > 0) {
+      fill(255, 255, 255, fadeOut);
+      noStroke();
+      textSize(92);
+      textAlign(CENTER, CENTER)
+      text('WINNER!', 10, 10, width - 10, height - 10);
+      fadeOut -= 10;
+    }
+
   }
 }
