7d3d742e2dfac6a0234a79d8d1bde84b9c2a629a
Michael <hux94handball@web.de>
2021-03-12 17:35:17 +0100

    CleanUp / Now Use fontNames.txt
    Did some cleaning.
    Playable fonts are now listed within the fontNames.txt file. Just enter a new line and the exact file name to add it to the game

--- sketch.js
+++ sketch.js
@@ -1,64 +1,22 @@
+// ################
+// Change Variables
+// ################
+const xMargin = 100;
+const yMarginTop = 75;
+const yMarginBottom = 250;
+const threshold = 35;
+let debugTrackingBoundaries = true;
+
+// ################
+// Helper Functions
+// ################
 let video;
 let poseNet;
 let pose;
 let fadeOut;
-let valueOne;
-let valueTwo;
+let endPtAONE;
+let endPtATWO;
 let reloadTimer;
-
-const xMargin = 100;
-const yMargin = 75;
-const treshHold = 100;
-const offSet = 35;
-reloadTimer = 0;
-
-// Choose random play font on each site reload
-var fontArray = [
-  "AngryU",
-  "Blinky",
-  "CircleAndSquare",
-  "FatSheriff",
-  "FlexK",
-  "InlineA",
-  "RotatO",
-  "SlantedQ",
-  "StachelBallon",
-  "WackelVudding",
-];
-var randomFont = fontArray[Math.floor(Math.random() * fontArray.length)];
-var playFont = new FontFace("playFont", "url(/fonts/" + randomFont + ".ttf)");
-playFont
-  .load()
-  .then(function (loaded_face) {
-    document.fonts.add(loaded_face);
-    document.body.style.fontFamily = "" + playFont + "";
-  })
-  .catch(function (error) {
-    console.log("Error.");
-  });
-
-function setup() {
-  var canvas = createCanvas(640, 480);
-
-  canvas.parent("canvas-holder");
-  video = createCapture(VIDEO);
-  video.hide();
-
-  // Set up the axes Settings from Bkg-Glyph to be put in the CSS-Property
-  valueOne = random(0, 1000);
-  valueTwo = random(0, 1000);
-  let newSettings = createCSSSettings(valueOne, valueTwo);
-  console.log("Current varSettings to be played are:", newSettings);
-  document
-    .getElementById("bkgGlyph")
-    .style.setProperty("font-variation-settings", newSettings);
-
-  poseNet = ml5.poseNet(video, "single", modelLoaded);
-  poseNet.on("pose", gotPose);
-
-  textSize(50);
-  textAlign(CENTER, CENTER);
-}
 
 function modelLoaded() {
   console.log("poseNet ready!");
@@ -70,63 +28,88 @@ function gotPose(poses) {
   }
 }
 
-// ################
-// Helper Functions
-// ################
+// Choose a random font on each reload
+// To add new fonts to the game, copy to fonts directory
+// and add a new line with the exact file name to fontNames.txt
+// TODO: Find an automated solution that reads from file directory
+function chooseActiveFont(result) {
+  let randomFont = random(result);
+  let activeFont = new FontFace("activeFont", "url(/fonts/" + randomFont + ")");
+  activeFont
+    .load()
+    .then(function (loaded_face) {
+      document.fonts.add(loaded_face);
+      document.body.style.fontFamily = "" + activeFont + "";
+    })
+    .catch(function (error) {
+      console.log("Error.");
+    });
+}
 
-function checkValues(axisOne, axisTwo) {
-  let statment1 = false;
-  let statment2 = false;
+function between(x, min, max) {
+  return x >= min && x <= max;
+}
 
-  if (axisOne >= valueOne - offSet && axisOne <= valueOne + offSet) {
-    statment1 = true;
+function checkValues(AONE, ATWO) {
+  // Check if the current user values
+  // are within the range of the end points +- threshold
+  let conditionAONE = false;
+  let conditionATWO = false;
+
+  if (between(AONE, endPtAONE - threshold, endPtAONE + threshold)) {
+    conditionAONE = true;
   }
-  if (axisTwo >= valueTwo - offSet && axisTwo <= valueTwo + offSet) {
-    statment2 = true;
+  if (between(ATWO, endPtATWO - threshold, endPtATWO + threshold)) {
+    conditionATWO = true;
   }
-  /*
-  console.log({
-    startValue1: valueOne,
-    currValue1: axisOne,
-    areClose1: statment1,
-    startValue2: valueTwo,
-    currValue2: axisTwo,
-    areClose2: statment2
-  })
-*/
-  if (statment1 && statment2) {
+  if (conditionAONE && conditionATWO) {
     return true;
   } else {
     return false;
   }
 }
 
-function distKeyPts(keyPtOne, keyPtTwo) {
+function distance(keyPtOne, keyPtTwo) {
   return dist(keyPtOne.x, keyPtOne.y, keyPtTwo.x, keyPtTwo.y);
 }
 
-function distKeyPtsX(keyPtOne, keyPtTwo) {
-  return dist(keyPtOne.x, 0, keyPtTwo.x, 0);
-}
-
-function angleKeyPts(keyPtOne, keyPtTwo) {
-  v1 = createVector(keyPtOne.x, keyPtOne.y);
-  // Angle still gives back weird values
-  // it seems to be the wrong calculation
-  return degrees(atan2(keyPtOne.x, keyPtOne.y));
-}
-
-function createCSSSettings(valueAxisOne, valueAxisTwo) {
-  // Set up the axes Settings to be put in the CSS-Property
-  let varSettings = '"AONE" ' + valueAxisOne;
+function createCSSSettings(valueAOne, valueATwo) {
+  // Set up the axes Settings to be put into the CSS-Property
+  let varSettings = '"AONE" ' + valueAOne;
   varSettings += ", ";
-  varSettings += '"ATWO" ' + valueAxisTwo;
+  varSettings += '"ATWO" ' + valueATwo;
   return varSettings;
 }
 
 // ################
 // Drawing Function
 // ################
+
+function preload() {
+  loadStrings("fontNames.txt", chooseActiveFont);
+}
+
+function setup() {
+  var canvas = createCanvas(640, 480);
+
+  canvas.parent("canvas-holder");
+  video = createCapture(VIDEO);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, "single", modelLoaded);
+  poseNet.on("pose", gotPose);
+
+  reloadTimer = 0;
+  // Evaluate random end points for each varFont axis
+  endPtAONE = random(0, 1000);
+  endPtATWO = random(0, 1000);
+  console.log(endPtAONE, endPtATWO);
+  let newSettings = createCSSSettings(endPtAONE, endPtATWO);
+  console.log("Current varSettings to be played are:", newSettings);
+  document
+    .getElementById("bkgGlyph")
+    .style.setProperty("font-variation-settings", newSettings);
+}
 
 function draw() {
   // Mirror the Video image
@@ -139,92 +122,65 @@ function draw() {
   image(video, 0, 0);
   filter(GRAY);
 
-  // Draw debuggin lines for Head tracking
-  stroke("#fff");
-  strokeWeight(5);
-  // Boundries for x-Tracking
-  // line(xMargin, 0, xMargin, height);
-  // line(width - xMargin, 0, width - xMargin, height);
+  if (debugTrackingBoundaries) {
+    // Draw debuggin lines for Head tracking
+    stroke("#fff");
+    strokeWeight(5);
+    // Boundries for x-Tracking
+    line(xMargin, 0, xMargin, height);
+    line(width - xMargin, 0, width - xMargin, height);
 
-  // Boundries for y-Tracking
-  // line(0, yMargin, width, yMargin);
-  // line(0, height - yMargin * 4, width, height - yMargin * 4);
+    // Boundries for y-Tracking
+    line(0, yMarginTop, width, yMarginTop);
+    line(0, height - yMarginBottom, width, height - yMarginBottom);
+  }
 
   if (pose) {
-    let distHands = distKeyPts(pose.leftWrist, pose.rightWrist);
-    // let axisTwo = map(distHands, xMargin, width - xMargin, 0, 1000, true)
+    // let distHands = distance(pose.leftWrist, pose.rightWrist);
 
-    let axisTwo = map(
-      pose.nose.y,
-      yMargin,
-      height - yMargin * 4,
+    // Currently the tracking is not good enough, so a hand distance of 0 will not be possible
+    // The maximum distance of hands needs to be calculate in a way based on the users arm length or so
+    // for now it is just an arbitrary value that was working with our first prototype setup at HS Trier
+    let minDistHands = 10;
+    let maxDistHands = 220;
+    let valueAONE = map(
+      distance(pose.leftWrist, pose.rightWrist),
+      minDistHands,
+      maxDistHands,
       0,
       1000,
       true
     );
-    // console.log(distHands, axisOne);
 
-    // Map the second Axes value to Nose-x position
-    let axisOne = map(distHands, 10, 220, 0, 1000, true);
+    let valueATWO = map(
+      pose.nose.y,
+      yMarginTop,
+      height - yMarginBottom,
+      0,
+      1000,
+      true
+    );
 
-    newSettings = createCSSSettings(axisOne, axisTwo);
+    // Create usable CSSSettings from the calculate values
+    varFontSettings = createCSSSettings(valueAONE, valueATWO);
     document
       .getElementById("activeGlyph")
-      .style.setProperty("font-variation-settings", newSettings);
+      .style.setProperty("font-variation-settings", varFontSettings);
 
-    // Draw Ellipse at Nose and Wrist Positions
-    strokeWeight(0);
-    // fill('#F55428');
-
+    // Draw Ellipse at nose and wrists positions
+    // as they are our current inputs
     fill("#fff");
     ellipse(pose.nose.x, pose.nose.y, 15);
-
-    fill("#fff");
     ellipse(pose.leftWrist.x, pose.leftWrist.y, 15);
     ellipse(pose.rightWrist.x, pose.rightWrist.y, 15);
 
-    /*
-     fill('#9A9B9F')
-     ellipse(pose.leftShoulder.x, pose.leftShoulder.y, 15);
-     ellipse(pose.rightShoulder.x, pose.rightShoulder.y, 15);
-    
-     ellipse(pose.leftShoulder.x, pose.leftShoulder.y, 15);
-     ellipse(pose.leftElbow.x, pose.leftElbow.y, 15);
-
-     ellipse(pose.leftHip.x, pose.leftHip.y, 15);
-     ellipse(pose.rightHip.x, pose.rightHip.y, 15);
-
-     ellipse(pose.leftKnee.x, pose.leftKnee.y, 15);
-     ellipse(pose.rightHip.x, pose.rightHip.y, 15);
-
-     ellipse(pose.leftAnkle.x, pose.leftAnkle.y, 15);
-     ellipse(pose.leftAnkle.x, pose.leftAnkle.y, 15);
-     */
-    /*
-        noFill();
-        stroke(255, 255, 255);
-        strokeWeight(2);
-        rect(50, 50, width - 100, height / 2);
-    */
-
-    if (checkValues(axisOne, axisTwo)) {
+    if (checkValues(valueAONE, valueATWO)) {
       fadeOut = 255;
       console.log(reloadTimer);
       if (reloadTimer > 300) {
         window.location.reload();
       }
       reloadTimer += 20;
-    }
-
-    if (fadeOut > 0) {
-      text("🤨", pose.nose.x, pose.nose.y);
-      fill(0, 0, 0, fadeOut);
-      noStroke();
-      text("😎", pose.nose.x, pose.nose.y);
-      fadeOut -= 45;
-    } else {
-      text("🤨", pose.nose.x, pose.nose.y);
-      reloadTimer = 0;
     }
 
     pop();
